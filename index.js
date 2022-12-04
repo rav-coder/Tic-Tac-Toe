495b66c6b589b8d7d2dee4009d22b8ffb0db0755
samohib <98845731+samohib@users.noreply.github.com>
2022-12-04 12:14:44 -0700

    Updated
    Shape class created and inherited by cross and circle class

--- index.js
+++ index.js
@@ -59,6 +59,102 @@ function loadGame(){
 }
 
 /**
+ * Holds the blueprint for a shape.
+ */
+ class Shape{
+
+  /**
+   * Get a associated canvas and 2d context to draw a shape.
+   * 
+   * @param {*} canvasId associated canvas with the id tag
+   */
+  constructor(canvasId){
+      this.canvas = document.getElementById(canvasId);
+      this.context = this.canvas.getContext("2d");
+  }
+
+  /**
+   * Assign necessary attributes before drawing a shape. 
+   */
+  begin(){
+    this.context.beginPath();
+    this.context.strokeStyle = 'firebrick';
+    this.context.lineWidth = 8;
+  }
+
+  /**
+   * End the drawing of a shape
+   */
+  end(){
+    this.context.stroke();
+  }
+
+}
+
+/**
+ * Holds blueprint for a cross 'X' which is a type of shape.
+ */
+class Cross extends Shape{
+  
+  /**
+   * Get a associated canvas to draw a cross and call the parent Shape constructor.
+   * 
+   * @param {*} canvasId associated canvas with the id tag
+   */
+  constructor(canvasId){
+    super(canvasId)
+  }
+
+  /**
+   * Draw the cross shape using two lines by specifying the start and end coordinates for a line.
+   */
+  draw(){
+
+    //Call the parent begin method before drawing and end method after drawing.
+    super.begin();
+
+    //make a line from top left to bottom right
+    this.context.moveTo(20, 20);
+    this.context.lineTo(80, 80);
+    super.end();
+
+    super.begin();
+
+    //make a line from top right to bottom left
+    this.context.moveTo(20, 80);
+    this.context.lineTo(80, 20);
+    super.end();
+  }
+
+}
+
+/**
+ * Holds the blueprint for circle 'O' which is a type of shape.
+ */
+class Circle extends Shape{
+
+ /**
+   * Get a associated canvas to draw a circle and call the parent Shape constructor.
+   * 
+   * @param {*} canvasId associated canvas with the id tag
+   */
+  constructor(canvasId){
+    super(canvasId)
+  }
+
+  /**
+   * Draw a circle specifying coordinates and 2 pi representing a full circle
+   */
+  draw(){
+    super.begin();
+    this.context.arc(50, 50, 35, 0, 2 * Math.PI);
+    super.end();
+  }
+
+}
+
+
+/**
  *  asdfadsf
  * @param {*} a dsaf
  * @param {*} b asdf
